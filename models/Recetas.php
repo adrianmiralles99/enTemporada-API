5e06217d769a8fd442784553464227b1fab198c4
Carlos <cdmarin@iesfuentesanluis.org>
2022-02-24 21:58:34 +0100

    a

--- models/Recetas.php
+++ models/Recetas.php
@@ -147,12 +147,4 @@ class Recetas extends \yii\db\ActiveRecord
         $this->pasos = json_decode($this->pasos, true); //Los convertimos desde Json a array de datos
         return parent::afterFind();
     }
-
-    public function beforeSave($insert)
-    {
-        $this->ingredientes = json_encode($this->ingredientes); //Los convertimos a Json
-        $this->pasos = json_encode($this->pasos); //Los convertimos a Json
-
-        return parent::beforeSave($insert);
-    }
 }
